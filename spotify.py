94a58bc4e16cee306220ddcfe7a919da3beb28f4
raysonbs <rayson.bs@hotmail.com>
2025-02-02 17:05:10 -0400

    consolidação projeto spotify

--- spotify.py
+++ spotify.py
@@ -1,10 +1,45 @@
 import streamlit as st 
 import pandas as pd
+import time  
 
 
-df = pd.read_csv('spotify.csv')
-df[df["Stream"] >  1000000]
-df
+st.set_page_config(
+    layout='wide',
+    page_title="Spotify Songs"
+)
 
-print(df.columns)
+@st.cache_data
+def load_data():
+    df = pd.read_csv('spotify.csv')
+    # st.session_state["df_spotify"] = df
+    time.sleep(5)
+    return df
+
+df = load_data()
+st.session_state["df_spotify"] = load_data()
+
+df.set_index("Track",inplace=True)
+
+
+artists = df['Artist'].value_counts().index
+artist = st.sidebar.selectbox('Artista',artists)
+df_filtered = df[df['Artist']==artist] 
+
+albuns = df_filtered['Album'].value_counts().index
+album = st.selectbox('Album',albuns)
+
+
+df_filtered2 = df[df['Album']==album] 
+
+col1, col2 = st.columns([0.7,0.3])
+
+col1.bar_chart(df_filtered2["Stream"])
+col2.line_chart(df_filtered2["Danceability"])
+
+
+
+
+
+st.write(artist)
+st.sidebar.button('test')
 
